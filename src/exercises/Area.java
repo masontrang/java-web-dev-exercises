333d93470dfad5ac954ca85fbada4ce31942bda0
masontrang <mt9260@gmail.com>
2022-01-03 19:47:00 -0500

    commit

--- src/exercises/Area.java
+++ src/exercises/Area.java
@@ -6,7 +6,6 @@ public class Area {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
         System.out.println("What is the length?");
         double length = input.nextDouble();
 
