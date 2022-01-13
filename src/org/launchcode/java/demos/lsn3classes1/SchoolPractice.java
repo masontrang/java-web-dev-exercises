235374212fc0f1c5e8a9806254f40ed72196dbd5
masontrang <mt9260@gmail.com>
2022-01-13 17:39:11 -0500

    commit

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,17 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student name = new Student();
+        name.setName("Mason");
+        name.setStudentId(12345);
+        name.setNumberOfCredits(1);
+        name.setGPA(4.0);
+
+
+        System.out.println("Name: " + name.getName());
+        System.out.println("Student ID: " + name.getStudentId());
+        System.out.println("Num of Credits: " + name.getNumberOfCredits());
+        System.out.println("GPA: " + name.getGPA());
+
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -2,12 +2,43 @@ package org.launchcode.java.demos.lsn3classes1;
 
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
-
 public class Student {
 
     private String name;
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int aStudentId) {
+        studentId = aStudentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public void setNumberOfCredits(int numOfCredits) {
+        numberOfCredits = numOfCredits;
+    }
+
+    public double getGPA() {
+        return gpa;
+    }
+
+    public void setGPA(double newGPA) {
+        gpa = newGPA;
+    }
 
 }
